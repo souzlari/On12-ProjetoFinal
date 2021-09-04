e43509fc4228ae606c02321d797423b85be459be
Lari Souza <larissaccsouza@gmail.com>
2021-09-04 13:58:28 -0300

    add config de port e db heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const app = require("./src/app");
-const port = 8080;
+const app = require("./src/app")
+const port = process.env.PORT || 8080
 
 app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
-});
+    console.log(`Servidor rodando na porta ${port}`)
+})
