c6a475a338ea2ae10b65a2ccd72868c7f781a7ee
Lari Souza <larissaccsouza@gmail.com>
2021-09-08 21:35:09 -0300

    @deus please

--- src/models/usersSchema.js
+++ src/models/usersSchema.js
@@ -15,4 +15,4 @@ const usersSchema = new mongoose.Schema({
   }
 });
 
-module.exports = mongoose.model('usuarias', usersSchema) 
+module.exports = mongoose.model('pessoasUsuarias', usersSchema) 
--- src/routes/library.routes.js
+++ src/routes/library.routes.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('../controllers/libraryControllers');
+const express = require('express')
+const router = express.Router()
+const controller = require("../controllers/libraryControllers")
 
-router.get('/', controller.getAll);
-router.get('/:id', controller.getById);
-router.post('/register', controller.createBook);
-router.patch('/update/:id', controller.updateBook);
-router.delete('/delete/:id', controller.deleteBook);
+router.get('/', controller.getAll)
+router.get("/:id", controller.getById)
+router.post('/register', controller.createBook)
+router.patch("/update/:id", controller.updateBook)
+router.delete("/delete/:id", controller.deleteBook)
 
-// router.get('/oi', (req, res)=>{
-//     res.status(200).send({'mensagem':'oi to aqui ta funcionando,'})
-// })
+router.get("/oi", (req, res)=>{
+    res.status(200).send({"mensagem":"oi to aqui ta funcionando "})
+})
 
 
 module.exports = router
